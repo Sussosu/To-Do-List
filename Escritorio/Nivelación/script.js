7811f4d3c64a11c47587aacc4d64b87af5ad1b68
Sussosu <susejcova1001@gmail.com>
2023-03-22 21:04:46 -0500

    git commit "changes"

---
+++ Escritorio/Nivelación/script.js
@@ -0,0 +1,18 @@
+//Variables ejercicio 1
+
+const priceProduct = 20 
+const iva = 19
+
+//calculo del precio junto con el iva
+const calc = priceProduct + (priceProduct + iva) / 100
+
+//ejecucion 
+console.log('Resultado: S' + calc)
+
+//Ejercicio 2
+
+let name = prompt("Escribe tu nombre")
+
+let saludo = 'Hola ' + name
+
+console.log(saludo)
